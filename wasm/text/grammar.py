fae9b5c329905c702a044eb88e572c7f06b7d07a
Piper Merriam <pipermerriam@gmail.com>
2019-03-05 11:25:07 -0700

    grammar

--- wasm/text/grammar.py
+++ wasm/text/grammar.py
@@ -56,38 +56,138 @@ digits = ~"[1-9][0-9]*"
 
 
 DONE = """
-num:    <digit> (_? <digit>)*
-hexnum: <hexdigit> (_? <hexdigit>)*
-nat:    <num> | 0x<hexnum>
-int:    <nat> | +<nat> | -<nat>
-float:  <num>.<num>?(e|E <num>)? | 0x<hexnum>.<hexnum>?(p|P <num>)?
-name:   $(<letter> | <digit> | _ | . | + | - | * | / | \ | ^ | ~ | = | < | > | ! | ? | @ | # | $ | % | & | | | : | ' | `)+
-string: "(<char> | \n | \t | \\ | \' | \" | \<hex><hex> | \u{<hex>+})*"
 
+func:    ( func <name>? <func_type> <local>* <instr>* )
+         ( func <name>? ( export <string> ) <...> )                         ;; = (export <string> (func <N>)) (func <name>? <...>)
+         ( func <name>? ( import <string> <string> ) <func_type>)           ;; = (import <name>? <string> <string> (func <func_type>))
+param:   ( param <val_type>* ) | ( param <name> <val_type> )
+result:  ( result <val_type>* )
+local:   ( local <val_type>* ) | ( local <name> <val_type> )
 
-value:  <int> | <float>
-var:    <nat> | <name>
+global:  ( global <name>? <global_type> <instr>* )
+         ( global <name>? ( export <string> ) <...> )                       ;; = (export <string> (global <N>)) (global <name>? <...>)
+         ( global <name>? ( import <string> <string> ) <global_type> )      ;; = (import <name>? <string> <string> (global <global_type>))
+table:   ( table <name>? <table_type> )
+         ( table <name>? ( export <string> ) <...> )                        ;; = (export <string> (table <N>)) (table <name>? <...>)
+         ( table <name>? ( import <string> <string> ) <table_type> )        ;; = (import <name>? <string> <string> (table <table_type>))
+         ( table <name>? ( export <string> )* <elem_type> ( elem <var>* ) ) ;; = (table <name>? ( export <string> )* <size> <size> <elem_type>) (elem (i32.const 0) <var>*)
+elem:    ( elem <var>? (offset <instr>* ) <var>* )
+         ( elem <var>? <expr> <var>* )                                      ;; = (elem <var>? (offset <expr>) <var>*)
+memory:  ( memory <name>? <memory_type> )
+         ( memory <name>? ( export <string> ) <...> )                       ;; = (export <string> (memory <N>))+ (memory <name>? <...>)
+         ( memory <name>? ( import <string> <string> ) <memory_type> )      ;; = (import <name>? <string> <string> (memory <memory_type>))
+         ( memory <name>? ( export <string> )* ( data <string>* ) )         ;; = (memory <name>? ( export <string> )* <size> <size>) (data (i32.const 0) <string>*)
+data:    ( data <var>? ( offset <instr>* ) <string>* )
+         ( data <var>? <expr> <string>* )                                   ;; = (data <var>? (offset <expr>) <string>*)
 
-unop:  ctz | clz | popcnt | ...
-binop: add | sub | mul | ...
-relop: eq | ne | lt | ...
+start:   ( start <var> )
 
-sign:  s|u
+typedef: ( type <name>? ( func <param>* <result>* ) )
+
+import:  ( import <string> <string> <imkind> )
+imkind:  ( func <name>? <func_type> )
+         ( global <name>? <global_type> )
+         ( table <name>? <table_type> )
+         ( memory <name>? <memory_type> )
+export:  ( export <string> <exkind> )
+exkind:  ( func <var> )
+         ( global <var> )
+         ( table <var> )
+         ( memory <var> )
+
+module:  ( module <name>? <typedef>* <func>* <import>* <export>* <table>? <memory>? <global>* <elem>* <data>* <start>? )
+         <typedef>* <func>* <import>* <export>* <table>? <memory>? <global>* <elem>* <data>* <start>?  ;; =
+         ( module <typedef>* <func>* <import>* <export>* <table>? <memory>? <global>* <elem>* <data>* <start>? )
 """
 
 
 grammar = parsimonious.Grammar(r"""
-integer_binops = integer_types "." binops
-num_const_op = valtype ".const"
+import = "import" _ string _ string _ imkind
+export = "export" _ string _ exkind
 
-binops =
+imkind =
+    ("func _ name? _ func_type) /
+    ("global _ name? _ global_type) /
+    ("table _ name? _ table_type) /
+    ("memory _ name? _ memory_type)
+exkind =
+    ("func _ var) /
+    ("global _ var) /
+    ("table _ var) /
+    ("memory _ var)
+
+func =
+    ("func" _ name? _ func_type _ local* _ instr*) /
+    ("func" _ name? _ ("export" _ string) <...> )                         ;; = (export <string> (func <N>)) (func <name>? <...>)
+    ("func" name? ("import" <string> <string> ) <func_type>)           ;; = (import <name>? <string> <string> (func <func_type>))
+
+expr =
+  op /
+  (op _ expr+) /
+  ("block" _ name? _ block_type _ instr*) /
+  ("loop" _ name? _ block_type _ instr*) /
+  ("if" _ name? _ block_type _ ("then" _ instr*) _ ("else" _ instr*)?) /
+  ("if" _ name? _ block_type _ expr+ _ ("then" _ instr*) ("else" instr*)?)
+
+instr =
+  expr /
+  op /
+  ("block" _ name? _ block_type _ instr* _ "end" _ name?) /
+  ("loop" _ name? _ block_type _ instr* _ "end" _ name?) /
+  ("if" _ name? _ block_type _ instr* _ "end" _ name?) /
+  ("if" _ name? _ block_type _ instr* "else" name? instr* "end" name? )
+
+op =
+  "unreachable" /
+  nop" /
+  ("br" _ var) /
+  ("br_if _ var) /
+  ("br_table _ var+) /
+  "return" /
+  ("call" _ var) /
+  ("call_indirect" _ func_type) /
+  "drop" /
+  "select" /
+  ("local.get" _ var) /
+  ("local.set" _ var) /
+  ("local.tee" _ var) /
+  ("global.get" _ var) /
+  ("global.set" _ var) /
+  (integer_types ".load" ("8" / "16" / "32")? "_" sign (_ offset)? (_ align)?) /
+  (float_types ".load" (_ offset?) (_ align)?) /
+  (integer_types ".store" ("8" / "16")? (_ offset)? (_ align)?) /
+  (float_types ".store" (_ offset)? (_ align)?) /
+  "memory.size" /
+  "memory.grow" /
+  (numeric_const_op _ value) /
+  numeric_op
+
+numeric_op =
+    float_ops /
+    integer_ops /
+    i32 ".wrap_" i64 /
+    i32 ".trunc_" float_types "_" sign /
+    i64 ".extend_" i32 "_" sign /
+    i64 ".trunc_" float_types "_" sign /
+    float_types ".convert_" integer_types "_" sign /
+    f32 ".demote_" f64 /
+    f64 ".promote" f32 /
+    i32 ".reinterpret_" f32 /
+    i64 ".reinterpret_" f64 /
+    f32 ".reinterpret_" i32 /
+    f64 ".reinterpret_" i64
+
+
+float_ops = float_types "." (float_unop_names | float_binop_names | float_relop_names)
+integer_ops = integer_types "." (integer_binop_names | integer_relop_names)
+numeric_const_op = valtype ".const"
+
+integer_binop_names =
     "add" /
     "sub" /
     "mul" /
-    "div_s" /
-    "div_u" /
-    "rem_s" /
-    "rem_u" /
+    "div_" sign /
+    "rem_" sign /
     "and" /
     "or" /
     "xor" /
@@ -96,8 +196,41 @@ binops =
     "shr_u" /
     "rotl" /
     "rotr"
+float_unop_names =
+    "abs" /
+    "neg" /
+    "ceil" /
+    "floor" /
+    "trunc" /
+    "nearest" /
+    "sqrt"
+float_binop_names =
+    "add" /
+    "sub" /
+    "mul" /
+    "div" /
+    "min" /
+    "max" /
+    "copysign"
+integer_relop_names =
+    "eqz" /
+    "eq" /
+    "ne" /
+    "lt_" sign /
+    "gt_" sign /
+    "le_" sign /
+    "ge_" sign
+float_relop_names =
+    "eq" /
+    "ne" /
+    "lt" /
+    "gt" /
+    "le" /
+    "ge"
 
 val_type = integer_types / float_types
+
+sign = "s" / "u"
 
 float_types = f32 / f64
 integer_types = i32 / i64
@@ -106,6 +239,17 @@ i32 = "i32"
 i64 = "i64"
 f32 = "f32"
 f64 = "f64"
+
+elem_type = "funcref"
+block_type = ("result" (_ val_type)*)*
+func_type:   ("type" _ var)? param* result*
+global_type: val_type | ("mut" val_type)
+table_type:  nat nat? elem_type
+memory_type: nat nat?
+
+param = ("param" (_ val_type)*) | ("param" _ name _ val_type)
+result = "result" (_ val_type)*
+local = ("local" (_ val_type)*) | ("local" _ name _ val_type)
 
 var = nat / name
 value = int / float
@@ -153,108 +297,10 @@ num = digit (_? digit)*
 digit = ~"[0-9]"
 
 
-unop = "ctz" / "clz" / "popcnt"
-relop = "eq" / "ne" / "lt_s" / "lt_u" / "gt_s" / "gt_u" / "le_s" / "le_u" / "ge_s" / "ge_u" / "lt" / "gt" / "le" / "ge"
-sign = "s" / "u"
-offset = "offset=" nat
 align = "align=" ("1" / "2" / "4" / "8" / "16" / "32")
-cvop = "trunc"
-
 offset: offset=<nat>
-align: align=(1|2|4|8|...)
-cvtop: trunc | extend | wrap | ...
 
-elem_type: funcref
-block_type : ( result <val_type>* )*
-func_type:   ( type <var> )? <param>* <result>*
-global_type: <val_type> | ( mut <val_type> )
-table_type:  <nat> <nat>? <elem_type>
-memory_type: <nat> <nat>?
 
-expr:
-  ( <op> )
-  ( <op> <expr>+ )                                                   ;; = <expr>+ (<op>)
-  ( block <name>? <block_type> <instr>* )
-  ( loop <name>? <block_type> <instr>* )
-  ( if <name>? <block_type> ( then <instr>* ) ( else <instr>* )? )
-  ( if <name>? <block_type> <expr>+ ( then <instr>* ) ( else <instr>* )? ) ;; = <expr>+ (if <name>? <block_type> (then <instr>*) (else <instr>*)?)
-
-instr:
-  <expr>
-  <op>                                                               ;; = (<op>)
-  block <name>? <block_type> <instr>* end <name>?                    ;; = (block <name>? <block_type> <instr>*)
-  loop <name>? <block_type> <instr>* end <name>?                     ;; = (loop <name>? <block_type> <instr>*)
-  if <name>? <block_type> <instr>* end <name>?                       ;; = (if <name>? <block_type> (then <instr>*))
-  if <name>? <block_type> <instr>* else <name>? <instr>* end <name>? ;; = (if <name>? <block_type> (then <instr>*) (else <instr>*))
-
-op:
-  unreachable
-  nop
-  br <var>
-  br_if <var>
-  br_table <var>+
-  return
-  call <var>
-  call_indirect <func_type>
-  drop
-  select
-  local.get <var>
-  local.set <var>
-  local.tee <var>
-  global.get <var>
-  global.set <var>
-  <val_type>.load((8|16|32)_<sign>)? <offset>? <align>?
-  <val_type>.store(8|16|32)? <offset>? <align>?
-  memory.size
-  memory.grow
-  <val_type>.const <value>
-  <val_type>.<unop>
-  <val_type>.<binop>
-  <val_type>.<testop>
-  <val_type>.<relop>
-  <val_type>.<cvtop>_<val_type>(_<sign>)?
-
-func:    ( func <name>? <func_type> <local>* <instr>* )
-         ( func <name>? ( export <string> ) <...> )                         ;; = (export <string> (func <N>)) (func <name>? <...>)
-         ( func <name>? ( import <string> <string> ) <func_type>)           ;; = (import <name>? <string> <string> (func <func_type>))
-param:   ( param <val_type>* ) | ( param <name> <val_type> )
-result:  ( result <val_type>* )
-local:   ( local <val_type>* ) | ( local <name> <val_type> )
-
-global:  ( global <name>? <global_type> <instr>* )
-         ( global <name>? ( export <string> ) <...> )                       ;; = (export <string> (global <N>)) (global <name>? <...>)
-         ( global <name>? ( import <string> <string> ) <global_type> )      ;; = (import <name>? <string> <string> (global <global_type>))
-table:   ( table <name>? <table_type> )
-         ( table <name>? ( export <string> ) <...> )                        ;; = (export <string> (table <N>)) (table <name>? <...>)
-         ( table <name>? ( import <string> <string> ) <table_type> )        ;; = (import <name>? <string> <string> (table <table_type>))
-         ( table <name>? ( export <string> )* <elem_type> ( elem <var>* ) ) ;; = (table <name>? ( export <string> )* <size> <size> <elem_type>) (elem (i32.const 0) <var>*)
-elem:    ( elem <var>? (offset <instr>* ) <var>* )
-         ( elem <var>? <expr> <var>* )                                      ;; = (elem <var>? (offset <expr>) <var>*)
-memory:  ( memory <name>? <memory_type> )
-         ( memory <name>? ( export <string> ) <...> )                       ;; = (export <string> (memory <N>))+ (memory <name>? <...>)
-         ( memory <name>? ( import <string> <string> ) <memory_type> )      ;; = (import <name>? <string> <string> (memory <memory_type>))
-         ( memory <name>? ( export <string> )* ( data <string>* ) )         ;; = (memory <name>? ( export <string> )* <size> <size>) (data (i32.const 0) <string>*)
-data:    ( data <var>? ( offset <instr>* ) <string>* )
-         ( data <var>? <expr> <string>* )                                   ;; = (data <var>? (offset <expr>) <string>*)
-
-start:   ( start <var> )
-
-typedef: ( type <name>? ( func <param>* <result>* ) )
-
-import:  ( import <string> <string> <imkind> )
-imkind:  ( func <name>? <func_type> )
-         ( global <name>? <global_type> )
-         ( table <name>? <table_type> )
-         ( memory <name>? <memory_type> )
-export:  ( export <string> <exkind> )
-exkind:  ( func <var> )
-         ( global <var> )
-         ( table <var> )
-         ( memory <var> )
-
-module:  ( module <name>? <typedef>* <func>* <import>* <export>* <table>? <memory>? <global>* <elem>* <data>* <start>? )
-         <typedef>* <func>* <import>* <export>* <table>? <memory>? <global>* <elem>* <data>* <start>?  ;; =
-         ( module <typedef>* <func>* <import>* <export>* <table>? <memory>? <global>* <elem>* <data>* <start>? )
 """)
 
 
